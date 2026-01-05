7582280113946260a78020dd096ddf5ece5452b4
maumogri2000-web <maumogri2000@gmail.com>
2026-01-05 14:28:00 +0100

    Add files via upload

---
+++ Appli Dispo pompier/service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "planning-2026-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
