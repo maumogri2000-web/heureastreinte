b90f9a52701e6efdd803a5dd4995e32ed5f148ee
maumogri2000-web <maumogri2000@gmail.com>
2026-01-07 12:14:36 +0100

    Update service-worker.js

--- Appli Dispo pompier/service-worker.js
+++ Appli Dispo pompier/service-worker.js
@@ -5,15 +5,17 @@ const FILES_TO_CACHE = [
   "./manifest.json"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
+self.addEventListener("install", event => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
